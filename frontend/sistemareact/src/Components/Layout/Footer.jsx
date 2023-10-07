31c3eb375897e58fb60dccad0375f74030f2e305
Pereira2002 <josepablopereira2002@gmail.com>
2023-10-06 19:11:57 -0600

    Footer y Header
    se agrego footer y header

--- frontend/sistemareact/src/Components/Layout/Footer.jsx
+++ frontend/sistemareact/src/Components/Layout/Footer.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+import '../../Asset/Css/Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBuilding, faIdCard, faMapMarkerAlt, faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
-    <div>
-      <h1>FOOTER</h1>
-    </div>
-  )
-}
+    <footer className="footer-container">
+      <div className="footer-content">
+        <div className="footer-section">
+          <div className="icon-text">
+            <FontAwesomeIcon icon={faBuilding} className="footer-icon" />
+            <h2 className="footer-heading">Transportes Castro G D JS.A</h2>
+          </div>
+          <div className="icon-text">
+            <FontAwesomeIcon icon={faIdCard} className="footer-icon" />
+            <p className="footer-text">Cédula 3-101-739593</p>
+          </div>
+        </div>
+        <div className="footer-section">
+          <div className="icon-text">
+            <FontAwesomeIcon icon={faMapMarkerAlt} className="footer-icon" />
+            <h3 className="footer-subheading">Dirección</h3>
+            <p className="footer-text">Río Frío 6, Urbanización Villa Nueva</p>
+            <p className="footer-text">Horquetas de Sarapiquí, Heredia</p>
+          </div>
+        </div>
+        <div className="footer-section">
+          <div className="icon-text">
+            <FontAwesomeIcon icon={faPhoneAlt} className="footer-icon" />
+            <p className="footer-text">
+              Teléfono: <a href="https://wa.me/50689927081" className="footer-link">8992-7081</a>
+            </p>
+            <FontAwesomeIcon icon={faEnvelope} className="footer-icon" />
+            <p className="footer-text">
+              Correo: <a href="mailto:transgdj@hotmail.com" className="footer-link">transgdj@hotmail.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- frontend/sistemareact/src/Components/Layout/Navbar.jsx
+++ frontend/sistemareact/src/Components/Layout/Navbar.jsx
@@ -1,37 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import '../../Asset/Css/Navbar.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'animate.css/animate.min.css'; // Importa las animaciones de Animate.css
 
 const Navbar = () => {
   return (
-    <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">STHC</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                    <a className="nav-link" href="/admin-inventario">Administrar Inventario</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/admin-camiones">Administrar Camiones</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="/admin-form-camiones">Pricing</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
-                </div>
-            </div>
-        </nav>
-
-
-    </div>
-  )
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">STHC</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav animate__animated animate__fadeInDown">
+            <li className="nav-item">
+              <a className="nav-link" href="/admin-inventario">Administrar Inventario</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/admin-camiones">Administrar Camiones</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/admin-form-camiones">Pricing</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
