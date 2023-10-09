3e9cee72d51775663ab3d9e0d235bfcc463e1ca8
Fernanda35Gonzalez <127688826+Fernanda35Gonzalez@users.noreply.github.com>
2023-10-09 15:11:18 -0600

    quitando titulo

--- frontend/sistemareact/src/Pages/Home.jsx
+++ frontend/sistemareact/src/Pages/Home.jsx
@@ -7,9 +7,8 @@ import Carrusel from '../Components/paginaPrincipal';
 const Home = () => {
   return (
     <Layout>
-        <br></br>
-        <h1 className='text-primary text-center'>Sistema de Transporte </h1>
-        <br></br>
+        
+        
         <Carrusel />
     </Layout>
   )
