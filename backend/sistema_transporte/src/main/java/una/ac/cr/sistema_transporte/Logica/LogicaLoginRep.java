2c975b1d7b6f13e7fdfd47a39b30bf2e455ce14d
Pereira2002 <josepablopereira2002@gmail.com>
2023-10-11 08:07:40 -0600

    recuperación contraseña

--- backend/sistema_transporte/src/main/java/una/ac/cr/sistema_transporte/Logica/LogicaLoginRep.java
+++ backend/sistema_transporte/src/main/java/una/ac/cr/sistema_transporte/Logica/LogicaLoginRep.java
@@ -4,7 +4,6 @@
  */
 package una.ac.cr.sistema_transporte.Logica;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import una.ac.cr.sistema_transporte.data.DataLogin;
@@ -23,32 +22,28 @@ public class LogicaLoginRep {
     public LogicaLoginRep(ServiceEmail serviceEmail) {
         this.serviceEmail = serviceEmail;
     }
-    
-    
-    
+
     public boolean iniciarRecuperacionContrasena(String email) {
-        
-    if (dataLogin.buscarEmail(email)) {
-        // Generar un token único
-        System.out.println("entrooooooo2");
-        String informacionUnica = "informacion-unica-para-generar-token";
-        String token = log.generarTokenUnico(informacionUnica);
-       
-        
-        // Asociar el token con el correo electrónico en la base de datos
-        dataLogin.asociarTokenRecuperacion(email, token);
 
-        // Enviar correo electrónico con el enlace de recuperación
-        String linkRecuperacion = "http://localhost:3000/admin-CambioContrasenia?token=" + token;
-        String cuerpoCorreo = "Haz clic en el siguiente enlace para recuperar tu contraseña: " + linkRecuperacion;
-        System.out.println(email+ "      " + cuerpoCorreo);
-        serviceEmail.enviarCorreo(email, "josepablopereira2002@gmail.com", "Recuperación de Contraseña", cuerpoCorreo);
+        if (dataLogin.buscarEmail(email)) {
+            // Generar un token único
+            System.out.println("entrooooooo2");
+            String informacionUnica = email;
+            String token = log.generarTokenUnico(informacionUnica);
 
-        return true;
-    } else {
-        return false;
+            // Asociar el token con el correo electrónico en la base de datos
+            dataLogin.asociarTokenRecuperacion(email, token);
+
+            // Enviar correo electrónico con el enlace de recuperación
+            String linkRecuperacion = "http://localhost:3000/admin-CambioContrasenia?token=" + token;
+            String cuerpoCorreo = "Haz clic en el siguiente enlace para recuperar tu contraseña: " + linkRecuperacion;
+            System.out.println(email + "      " + cuerpoCorreo);
+            serviceEmail.enviarCorreo(email, "josepablopereira2002@gmail.com", "Recuperación de Contraseña", cuerpoCorreo);
+
+            return true;
+        } else {
+            return false;
+        }
     }
-}
 
-    
 }
--- backend/sistema_transporte/src/main/java/una/ac/cr/sistema_transporte/service/ServiceEmail.java
+++ backend/sistema_transporte/src/main/java/una/ac/cr/sistema_transporte/service/ServiceEmail.java
@@ -1,8 +1,10 @@
 package una.ac.cr.sistema_transporte.service;
 
+import java.util.Properties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,11 +18,20 @@ public class ServiceEmail {
     }
 
     public void enviarCorreo(String correoTo, String correoFrom, String titulo, String cuerpo) {
+        Properties props = new Properties();
+        props.setProperty("mail.smtp.ssl.protocols", "TLSv1.2"); // Configura TLS v1.2
+        props.setProperty("mail.smtp.ssl.enable", "true");
+
         SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(correoTo);
         email.setSubject(titulo);
         email.setText(cuerpo);
-        mail.send(email);
+
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setJavaMailProperties(props);
+
+   
+        sender.send(email);
     }
     
 }
