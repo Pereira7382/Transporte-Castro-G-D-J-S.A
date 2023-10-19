b01970026f56b80d201e197b980e3def7acad682
Dilan_GC <132763829+jeffrybrenesUNA@users.noreply.github.com>
2023-10-19 10:16:15 -0600

    correciones

--- backend/sistema_transporte/src/main/java/una/ac/cr/sistema_transporte/Logica/LogicaLogin.java
+++ backend/sistema_transporte/src/main/java/una/ac/cr/sistema_transporte/Logica/LogicaLogin.java
@@ -17,32 +17,35 @@ import java.util.LinkedList;
 import una.ac.cr.sistema_transporte.data.DataLogin;
 import una.ac.cr.sistema_transporte.domain.Usuario;
 import una.ac.cr.sistema_transporte.security.Encriptacion;
+
 @Component
 public class LogicaLogin {
 
     Encriptacion encriptacion = new Encriptacion();
     DataLogin dataLogin = new DataLogin();
-    
-    public boolean validarAcceso(Usuario usuario){
+
+    public boolean validarAcceso(Usuario usuario) {
         //recuperar lista de usuarios registrados en la base de datos.
         LinkedList<Usuario> usuarios = new LinkedList<Usuario>();
         usuarios = dataLogin.obtenerUsuarios();
         //recorro la lista de usuarios y si el metodo de comparar password del usuario actual de la lista
         // con el usuario que se pasa por parametro retorna true quiere decir que hay un usuario con las credenciales
-        
-        for(Usuario us : usuarios){
+
+        for (Usuario us : usuarios) {
             //primero ver el usuario, si el usuario es igual al que se esta bsucando se compara contraseñas
-            if(us.getUsuario().equals(usuario.getUsuario())){
+            if (us.getUsuario().equals(usuario.getUsuario())) {
                 //comparamos contraseñas, si las contrasñas coinciden entonces slaimos con true;
-                if(encriptacion.compararPasswords( usuario.getClave(), us.getClave())){
+                if (encriptacion.compararPasswords(usuario.getClave(), us.getClave())) {
                     return true;
                 }
             }
         }
         return false;
     }
-    
-    public boolean agregarUsuario(Usuario usuario){
+
+    public boolean agregarUsuario(Usuario usuario) {
+        System.out.println("\n llego a la logica a registrar nuevo usuario ");
+
         boolean agregado = false;
         //encriptar la contraseña
         String contraseniaEncriptada = "";
@@ -55,8 +58,8 @@ public class LogicaLogin {
         return agregado;
 
     }
-  
- public String generarTokenUnico(String informacionUnica) {
+
+    public String generarTokenUnico(String informacionUnica) {
         String claveSecreta = "a1b2C3d4E5f6G7h8I9j0KlMnOpQrStUvWxYzAqBpCrDsEtFuGvHwIxJyKzLmNoPjQkRlSmTnUoVpWqXrYsZtUvWxYzA1B2C3D4E5F6G7H8I9J0K1L2M3N4O5P6Q7R8S9T0U1V2W3X4Y5ZaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
         byte[] claveBytes = claveSecreta.getBytes(StandardCharsets.UTF_8);
         Key jwtSecret = new SecretKeySpec(claveBytes, SignatureAlgorithm.HS256.getJcaName());
